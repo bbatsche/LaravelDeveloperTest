db68e12d50b2641e5971b310900672bf65d8f29b
Ben Batschelet <bartleby84@gmail.com>
2025-11-20 11:46:43 -0600

    Create profile migrations

---
+++ database/migrations/2025_11_20_165601_create_profiles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Create `profiles` table for `/users` API endpoint.
+ *
+ * Didn't want to risk overwriting the existing `users` table, although I'm not
+ * likely to use it for this project.
+ */
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            // Could probably be considered the primary key, but safer to let MySQL handle it
+            $table->integer('profile_id', unsigned: true)->unique();
+            $table->string('name', 191);
+            $table->string('username', 191)->unique();
+            $table->string('email', 191)->index();
+            // Sample data appears to be North American numbers (although with some weird inconsistencies)
+            $table->char('phone', 10);
+            $table->string('extension', 8)->nullable();
+            $table->string('website');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
---
+++ database/migrations/2025_11_20_173040_create_addresses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('profile_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('string', 191);
+            $table->string('suite', 191)->nullable();
+            $table->string('city', 191);
+            $table->char('zip', 5);
+            $table->char('zip4', 4)->nullable();
+            $table->float('latitude');
+            $table->float('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
